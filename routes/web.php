fede67d54a16a47a509c5e2b2ac68f2411805672
Jhale-x <tu-email-de-jhale@gmail.com>
2026-02-04 20:07:09 -0500

    Avance: web

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('web_principal');
+});
+
+Route::get('/intranet', function () {
+    return view('intranet');
 });
