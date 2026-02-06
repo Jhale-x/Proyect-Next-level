e2cb83e050811e4c3300cfde0642dddac7fda5c5
ManuelAnto123 <manuelx345antonio@gmail.com>
2026-02-05 20:26:35 -0500

    Avance: todo subido ya, juntamos codigo

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-<<<<<<< HEAD
 use Illuminate\Support\Facades\Route; // Siempre asegúrate de tener esta línea
 use App\Http\Controllers\ActividadController;
 use App\Http\Controllers\CursosController;
@@ -58,33 +57,3 @@ Route::prefix('usuarios')->group(function () {
 |--------------------------------------------------------------------------
 */
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-=======
-use App\Http\Controllers\homeController;
-use App\Http\Controllers\ActivityController;
-use App\Http\Controllers\herramientasController;
-use App\Http\Controllers\organizacionesController;
-use App\Http\Controllers\soporteController;
-use App\Http\Controllers\mensajesController;
-use App\Http\Controllers\MensajessController;
-use App\Http\Controllers\calificacionesController;
-use App\Http\Controllers\asistenciaController;
-use App\Http\Controllers\asispController;
-use Illuminate\Support\Facades\Route;
-
-
-Route::get('/', function () {
-    return view('home');
-});
-
-
-//Secciones
-Route::get('/Activity', [ActivityController::class, 'index'])->name('Activity');
-Route::get('/herramientas', [herramientasController::class, 'index'])->name('herramientas');
-Route::get('/organizaciones', [organizacionesController::class, 'index'])->name('organizaciones');
-Route::get('/mensajes', [mensajesController::class, 'index'])->name('mensajes');
-Route::get('/soporte', [soporteController::class, 'index'])->name('soporte');
-Route::get('/mensajes/nuevo/{curso}', [MensajessController::class, 'create'])->name('mensajes.nuevo');
-Route::get('/calificaciones', [calificacionesController::class, 'index'])->name('calificaciones');
-Route::get('/asistencia', [asistenciaController::class, 'index'])->name('asistencia');
-Route::get('/asistenciaP', [asispController::class, 'index'])->name('asistenciaP');
->>>>>>> 6449e0ffd2cda145b29f11a55a06e18829329855
